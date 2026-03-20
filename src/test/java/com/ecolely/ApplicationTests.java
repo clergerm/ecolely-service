cfa39d666e949a55b1069b1f00b336d8933f9aea
Martin <clergerm@gmail.com>
2026-03-20 18:38:30 -0400

    Adding Copyright

--- src/test/java/com/ecolely/ApplicationTests.java
+++ src/test/java/com/ecolely/ApplicationTests.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2026 Ecolely. All rights reserved.
+ * Unauthorized copying, modification, distribution, or use of this software is strictly prohibited.
+ */
+
 package com.ecolely;
 
 import org.junit.jupiter.api.Test;
