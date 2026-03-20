13786e6413bbfe83ace7559f348e0073364cfd91
Martin <clergerm@gmail.com>
2026-03-20 17:01:40 -0400

    Initial commit

---
+++ src/main/java/com/ecolely/Application.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2026 Ecolely. All rights reserved.
+ * Unauthorized copying, modification, distribution, or use of this software is strictly prohibited.
+ */
+
+package com.ecolely;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {SpringApplication.run(Application.class, args);}
+
+}
---
+++ src/main/java/com/ecolely/controller/HealthController.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2026 Ecolely. All rights reserved.
+ * Unauthorized copying, modification, distribution, or use of this software is strictly prohibited.
+ */
+
+package com.ecolely.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+    @GetMapping("/")
+    public String healthCheck() {
+        return "Ecolely is running!";
+    }
+}
