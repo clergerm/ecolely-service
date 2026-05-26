a5d4f63fec39b1d1a2408eeeecaf76491e1de3fd
Martin Clerger <clergerm@gmail.com>
2026-05-26 18:18:06 -0400

    Renaming main class

--- src/test/java/com/ecolely/EcolelyApplicationTests.java
+++ src/test/java/com/ecolely/EcolelyApplicationTests.java
@@ -9,7 +9,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ApplicationTests {
+class EcolelyApplicationTests {
 
 	@Test
 	void contextLoads() {
